c3148aeea32edd52f3fb0ad70b7635dceae154bf
Ivanmamaev1 <mamaevivan92@gmail.com>
2024-09-24 10:36:58 +0300

    Making interface. Start to coding logic of the Traffic Light. Add breakpoint.

--- HomeWork2.02/ViewController.swift
+++ HomeWork2.02/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 50
+        switchButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func switchButtonDidTapped(_ sender: UIButton) {
+        redLightView.alpha = 1
+        sender.setTitle("NEXT", for: .normal)
+    }
+    
 }
-
